7113c1d1a4dda6851610b547428966ccdaa822fe
Darren-Gavin Ho <darren080589@gmail.com>
2013-11-12 00:46:00 +0800

    EditMovieView Implementations

--- app/assets/javascripts/movie_app.js
+++ app/assets/javascripts/movie_app.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 		url: "http://cs3213.herokuapp.com/movies.json?page=1",
 		changeUrl: function(i){page = i; this.url=baseUrl+page;}
 	});
+
 	var IndexView = Barebone.View.extend({
 		setup: function(){
 			this.el = "pageBody";
@@ -19,9 +20,31 @@ $(document).ready(function(){
 			this.$el().html("");
 			for(var i=0; i<this.myMovieList.attributes.length; i++){
 				var renderString = "<a id='" + this.myMovieList.get("id", i) + "' class='MovieSimpleLink' href='javascript: void(0);'><b><u>"+this.myMovieList.get("title", i)+"</u></b></a><br /><br />";
-    renderString += "<a id='" + this.myMovieList.get("id", i) + "' class='MovieSimpleImg' href='javascript: void(0);'><div><img src='" + this.myMovieList.get("img_url", i) +"' /></div></a>";
-    		this.$el().append(renderString);
+    			renderString += "<a id='" + this.myMovieList.get("id", i) + "' class='MovieSimpleImg' href='javascript: void(0);'><div><img src='" + this.myMovieList.get("img_url", i) +"' /></div></a>";
+    			this.$el().append(renderString);
 			}
+		},
+	});
+
+	var EditMovieView = Barebone.View.extend({
+
+		setup: function() {
+			this.registerDomEvents();
+			this.render();
+		},
+
+		events: {
+			"click #updateMovieBtn" : "updateMovie"
+		},		
+
+		updateMovie: function() {
+			this.event.trigger("change_page", null, {page: "userMovies"});
+		},
+
+		render: function() {
+			this.$el().html("");
+			var renderString = "<button id='updateMovieBtn'>Update Movie</button>";
+			this.$el().append(renderString);
 		},
 	});
 
@@ -43,27 +66,38 @@ $(document).ready(function(){
 			if(options.page == "createMovies")
 				this.showCreateMovies();
 
-			if(options.page == "updateMovie")
-				this.showUpdateMovie();
+			if(options.page == "editMovie")
+				this.showEditMovie();
 
+			if(options.page == "movieDetail")
+				this.showMovieDetail();
 
 		},
 
+		//Shows the index page
 		showIndex: function() {
 			this.current_view = new IndexView();
 			this.current_view.setup();
-			this.event.trigger("change_page", null, {page: "userMovies"});
 		},
 
+		//Shows all User Movies
 		showUserMovies: function() {
 			console.log("User Movies are shown here");
 		},
 
+		//Shows the form to create a new Movie
 		showCreateMovies: function() {
 
 		},
 
-		showUpdateMovie: function() {
+		//Show the form to update the Movie
+		showEditMovie: function() {
+			this.current_view = new EditMovieView();
+			this.current_view.setup();
+		},
+
+		//Shows the Movie's details and its reviews
+		showMovieDetail: function() {
 
 		},
 	});
