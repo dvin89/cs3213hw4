5f938f43735ea1fc2fcefa7529a15eaaba6381a6
ecc-weizhi <ecc.weizhi@gmail.com>
2013-11-12 01:00:59 +0800

    add create movie page

--- app/assets/javascripts/movie_app.js
+++ app/assets/javascripts/movie_app.js
@@ -1,12 +1,54 @@
 $(document).ready(function(){
 
 	//classes declaration
+	var NewMovie = Barebone.Model.extend({
+    url: "http://cs3213.herokuapp.com/movies.json",
+  });
+	
 	var MovieList = Barebone.Model.extend({
 		baseUrl: "http://cs3213.herokuapp.com/movies.json?page=",
 		page: 1,
 		url: "http://cs3213.herokuapp.com/movies.json?page=1",
 		changeUrl: function(i){page = i; this.url=baseUrl+page;}
 	});
+	
+	var CreateMovieView = Barebone.View.extend({
+		setup: function(){
+			this.el = "pageBody";
+			this.render();
+		},	
+		
+		render: function(){
+			this.$el().html("");
+			var renderString = "<div style='margin-left: 20px;' id='movieForm'><form id='createMovieForm' name='movie' method='POST'>";
+    	renderString += "<table><tr>";
+   		renderString += "<td colspan=2><h1>Create new movie</h1></td></tr>";
+    	renderString += "<tr><td>Title: </td><td><input type='text' name='movie[title]' id='movie_title'></td></tr>";
+    	renderString += "<tr><td>Summary: </td><td> <input type='text' name='movie[summary]' id='summary'></td></tr>";
+    	renderString += "<tr><td>Image: </td><td> <input type='file' name='movie[img]' id='movie_img'></td></tr>";
+    	renderString += "<tr><td colpsan=2  style='text-align: center;'><button class='btn btn-primary' id='Create' type='button'>Create</button></td></tr></table></form></div>";
+    
+    	this.$el().append(renderString);
+    	this.registerDomEvents();
+		},
+		
+		events: {
+			"click #Create": "create"
+		},
+		
+		create: function(){
+			alert("in cre8");
+			var myNewMovie = new NewMovie();
+			myNewMovie.event.on("change", this.hookToMovieDetail, this);
+			myNewMovie.save(document.getElementById("createMovieForm"), {"access_token": gon.token});
+		},
+		
+		hookToMovieDetail: function(){
+			//this is supposed to be a function to link to a page showing the newly created movie details
+		}
+	
+	});
+	
 	var IndexView = Barebone.View.extend({
 		setup: function(){
 			this.el = "pageBody";
@@ -28,8 +70,12 @@ $(document).ready(function(){
 
 
 	//code here
-	var myIndexView = new IndexView();
-	myIndexView.setup();
+	//var myIndexView = new IndexView();
+	//myIndexView.setup();
+	
+	var mycreatemovie = new CreateMovieView();
+	mycreatemovie.setup();
+
 
 
 
